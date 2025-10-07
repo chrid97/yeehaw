09dc131b136a7b0c3293d7adc09532e2fc9cbe7e
chrid97 <cgoncal12@gmail.com>
2025-10-06 22:55:56 -0700

    Trying to setup some isometric projection; Drew tiles around the road to help visualzie it

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include "raylib.h"
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#define MAX_OBSTACLES 100
 
 const int VIRTUAL_WIDTH = 800;
 const int VIRTUAL_HEIGHT = 450;
@@ -26,12 +28,20 @@ int main(void) {
       .width = PLAYER_WIDTH,
       .height = PLAYER_HEIGHT,
       .pos = {.x = 0, .y = VIRTUAL_HEIGHT - PLAYER_HEIGHT},
-      .velocity.x = 4,
-      .velocity.y = 4,
+      .velocity.x = 1,
+      .velocity.y = 1,
   };
 
   int map[90] = {0};
 
+  float worldOffset = 0.0f;
+  float worldSpeed = 300.0f;
+  float scroll = fmodf(worldOffset, VIRTUAL_WIDTH);
+
+  Entity obstacles[MAX_OBSTACLES] = {0};
+  for (int i = 0; i < MAX_OBSTACLES; i++) {
+    obstacles[i] = (Entity){.pos.x = i * 70, .pos.y = 340};
+  }
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
     // Screen scaling
@@ -46,16 +56,27 @@ int main(void) {
     // ---- Input ---- //
     // --------------- //
     if (IsKeyDown(KEY_D)) {
-      player.pos.x += player.velocity.x;
+      player.velocity.x = player.velocity.x;
     }
 
     if (IsKeyDown(KEY_A)) {
-      player.pos.x -= player.velocity.x;
+      player.velocity.x = -player.velocity.x;
     }
+
+    if (IsKeyDown(KEY_W)) {
+      player.pos.y -= player.velocity.y;
+    }
+
+    if (IsKeyDown(KEY_S)) {
+      player.pos.y += player.velocity.y;
+    }
+
+    player.pos.x += player.velocity.x;
 
     // ---------------- //
     // ---- Update ---- //
     // ---------------- //
+    worldOffset += worldSpeed * dt;
 
     // ---------------- //
     // ----- Draw ----- //
@@ -63,12 +84,24 @@ int main(void) {
     BeginDrawing();
     ClearBackground((Color){235, 200, 150, 255});
 
-    for (int i = -10; i < 50; i++) {
-      DrawRectangle(i * 200, 300 + 40, 200, 100, (Color){194, 178, 128, 255});
+    // GROUND
+    DrawRectangle(0, 250, VIRTUAL_WIDTH, 150, (Color){194, 178, 128, 255});
+    for (int y = 250; y < 250 + 150; y += 50) {
+      for (int x = 0; x < VIRTUAL_WIDTH; x += 50) {
+        DrawRectangleLines(x, y, 50, 50, RED);
+      }
     }
 
     DrawRectangle(player.pos.x, player.pos.y, player.width, player.height,
                   BLUE);
+
+    // for (int i = 0; i < MAX_OBSTACLES; i++) {
+    //   DrawRectangle(obstacles[i].pos.x, obstacles[i].pos.y, 25, 60,
+    //   DARKGREEN);
+    // }
+    // CACTUS
+    // DrawRectangle(400, 260, 20, 60, DARKGREEN);
+    // DrawRectangle(400, 340, 20, 60, DARKGREEN);
 
     EndDrawing();
   }
