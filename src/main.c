baebd23f25f847f16c795816cadce65e85432ebd
chrid97 <cgoncal12@gmail.com>
2025-10-07 22:06:49 -0700

    drawing squares as isometric tiles

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 #define MAX_OBSTACLES 100
@@ -13,12 +12,39 @@ const int VIRTUAL_WIDTH = 800;
 const int VIRTUAL_HEIGHT = 450;
 const int PLAYER_WIDTH = 50;
 const int PLAYER_HEIGHT = 40;
+const int TILE_WIDTH = 100;
+const int TILE_HEIGHT = 50;
 
 void test(char *text) {
   DrawText(text, GetScreenWidth() / 2, GetScreenHeight() / 2, 100, RED);
 }
 
-int random_between(int min, int max) { return min + rand() % (max - min + 1); }
+int x_start = VIRTUAL_WIDTH / 2 - TILE_WIDTH / 2;
+int y_start = 50;
+void draw_tile(Vector2 pos) {
+  int half_width = TILE_WIDTH / 2;
+  int half_height = TILE_HEIGHT / 2;
+
+  int screen_x = x_start + (pos.x - pos.y) * half_width;
+  int screen_y = y_start + (pos.x + pos.y) * half_height;
+
+  // DrawRectangleLines(screen_x, screen_y, TILE_WIDTH, TILE_HEIGHT, BLACK);
+  Vector2 top = {.x = screen_x + half_width, screen_y};
+  Vector2 left = {.x = screen_x, screen_y + half_height};
+  Vector2 right = {.x = screen_x + TILE_WIDTH, screen_y + half_height};
+  Vector2 bottom = {.x = screen_x + half_width, screen_y + TILE_HEIGHT};
+  // DrawCircle(right.x, right.y, 5, RED);
+  // DrawCircle(top.x, top.y, 5, RED);
+  // DrawCircle(left.x, left.y, 5, RED);
+  // DrawCircle(bottom.x, bottom.y, 5, RED);
+
+  DrawTriangle(top, left, right, GRAY);
+  DrawTriangle(bottom, right, left, GRAY);
+  DrawLineV(top, left, BLACK);
+  DrawLineV(top, right, BLACK);
+  DrawLineV(bottom, right, BLACK);
+  DrawLineV(bottom, left, BLACK);
+}
 
 int main(void) {
   InitWindow(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, "Yeehaw");
@@ -85,15 +111,15 @@ int main(void) {
     ClearBackground((Color){235, 200, 150, 255});
 
     // GROUND
-    DrawRectangle(0, 250, VIRTUAL_WIDTH, 150, (Color){194, 178, 128, 255});
-    for (int y = 250; y < 250 + 150; y += 50) {
-      for (int x = 0; x < VIRTUAL_WIDTH; x += 50) {
-        DrawRectangleLines(x, y, 50, 50, RED);
-      }
-    }
-
-    DrawRectangle(player.pos.x, player.pos.y, player.width, player.height,
-                  BLUE);
+    // DrawRectangle(0, 250, VIRTUAL_WIDTH, 150, (Color){194, 178, 128, 255});
+    // for (int y = 250; y < 250 + 150; y += 50) {
+    //   for (int x = 0; x < VIRTUAL_WIDTH; x += 50) {
+    //     DrawRectangleLines(x, y, 50, 50, RED);
+    //   }
+    // }
+    //
+    // DrawRectangle(player.pos.x, player.pos.y, player.width, player.height,
+    //               BLUE);
 
     // for (int i = 0; i < MAX_OBSTACLES; i++) {
     //   DrawRectangle(obstacles[i].pos.x, obstacles[i].pos.y, 25, 60,
@@ -102,6 +128,51 @@ int main(void) {
     // CACTUS
     // DrawRectangle(400, 260, 20, 60, DARKGREEN);
     // DrawRectangle(400, 340, 20, 60, DARKGREEN);
+
+    // int start = screen_width / 2;
+    // // DrawRectangleLines(start, 0, TILE_WIDTH, TILE_HEIGHT, BLACK);
+    //
+    // Vector2 top = {.x = start + (TILE_WIDTH / 2), .y = 0};
+    // Vector2 right = {.x = start + TILE_WIDTH, .y = TILE_HEIGHT / 2};
+    // Vector2 left = {.x = start, .y = TILE_HEIGHT / 2};
+    // Vector2 bottom = {.x = start + TILE_WIDTH / 2, .y = TILE_HEIGHT};
+    //
+    // DrawTriangle(right, top, bottom, GRAY);
+    // DrawTriangle(top, left, bottom, GRAY);
+    // DrawLineV(top, left, BLACK);
+    // DrawLineV(top, right, BLACK);
+    // DrawLineV(bottom, left, BLACK);
+    // DrawLineV(bottom, right, BLACK);
+    //
+    // // DrawRectangleLines(start + TILE_WIDTH / 2, TILE_HEIGHT / 2,
+    // TILE_WIDTH,
+    // //                    TILE_HEIGHT, BLACK);
+    // Vector2 top2 = {.x = top.x + TILE_WIDTH / 2, .y = top.y + TILE_HEIGHT /
+    // 2}; Vector2 right2 = {.x = right.x + (TILE_WIDTH / 2),
+    //                   .y = right.y + TILE_HEIGHT / 2};
+    // Vector2 left2 = {.x = left.x + TILE_WIDTH / 2,
+    //                  .y = left.y + (TILE_HEIGHT / 2)};
+    // Vector2 bottom2 = {.x = bottom.x + TILE_WIDTH / 2,
+    //                    .y = bottom.y + (TILE_HEIGHT / 2)};
+    //
+    // DrawTriangle(right2, top2, bottom2, GRAY);
+    // DrawTriangle(top2, left2, bottom2, GRAY);
+    // DrawLineV(top2, left2, BLACK);
+    // DrawLineV(top2, right2, BLACK);
+    // DrawLineV(bottom2, left2, BLACK);
+    // DrawLineV(bottom2, right2, BLACK);
+
+    draw_tile((Vector2){.x = 0, .y = 0});
+
+    draw_tile((Vector2){.x = 1, .y = 0});
+    draw_tile((Vector2){.x = 2, .y = 0});
+
+    draw_tile((Vector2){.x = 0, .y = 1});
+    draw_tile((Vector2){.x = 0, .y = 2});
+
+    draw_tile((Vector2){.x = 1, .y = 1});
+    draw_tile((Vector2){.x = 2, .y = 2});
+    draw_tile((Vector2){.x = 2, .y = 1});
 
     EndDrawing();
   }
