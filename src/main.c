f57d80fc5563372cc8528190c6d425cff2db7b54
chrid97 <cgoncal12@gmail.com>
2025-10-10 20:23:04 -0700

    Spawn tiles around player; Add rock and player collision

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
@@ -29,6 +30,7 @@ static Camera2D camera;
 static Texture2D tile000;
 static Texture2D player_sprite;
 static Texture2D tile_wall;
+static Texture2D rock_texture;
 
 static Music bg_music;
 static Sound hit_sound;
@@ -43,6 +45,7 @@ void init_game(void) {
   player_sprite = LoadTexture("assets/horse.png");
   tile_wall = LoadTexture("assets/tileset/tile_057.png");
   tile000 = LoadTexture("assets/tileset/tile_000.png");
+  rock_texture = LoadTexture("assets/tileset/tile_055.png");
 
   // Load Music
   bg_music = LoadMusicStream("assets/spagetti-western.ogg");
@@ -54,11 +57,14 @@ void init_game(void) {
   SetSoundVolume(hit_sound, 0.5f);
 
   // Init Game Objects
-  player = (Entity){
-      .pos.x = 0, .pos.y = 0, .width = 32, .height = 32, .color = WHITE};
+  player =
+      (Entity){.pos.x = 0, .pos.y = 0, .width = 1, .height = 1, .color = WHITE};
   camera = (Camera2D){.target = (Vector2){0, 0},
                       .offset = (Vector2){0, 0},
                       .rotation = 0.0f,
+                      // (TODO) Add button screen rotation to test
+                      // rotation 10 doesnt look terrible maybe ill use that
+                      // .rotation = 10.0f,
                       .zoom = 1.0f};
 }
 
@@ -92,6 +98,19 @@ void update_draw(void) {
   camera.target = player_screen;
   camera.offset = (Vector2){GetScreenWidth() / 4.0f, GetScreenHeight() / 1.5f};
 
+  Entity cactus = {.pos.x = 0, .pos.y = -10, .width = 1, .height = 1};
+  Rectangle cactus_rect = {cactus.pos.x, cactus.pos.y, cactus.width,
+                           cactus.height};
+  Rectangle player_rect = {player.pos.x, player.pos.y, player.width,
+                           player.height};
+
+  if (CheckCollisionRecs(cactus_rect, player_rect)) {
+    printf("%f\n", player_rect.x);
+    player.color = RED;
+  } else {
+    player.color = WHITE;
+  }
+
   // --- Draw ---
   BeginDrawing();
   ClearBackground(ORANGE);
@@ -112,13 +131,18 @@ void update_draw(void) {
 
   // Draw world
   // (NOTE) figure out how to start from 0 instead of a negative number
-  for (int y = -200; y < 30; y++) {
+  int tile_y = floorf(player.pos.y);
+  for (int y = tile_y - 30; y < tile_y + 14; y++) {
     for (int x = -5; x < 8; x++) {
       Vector3 world = {x, y, 0};
       Vector2 screen = isometric_projection(world);
       DrawTextureV(tile000, screen, WHITE);
     }
   }
+
+  Vector2 object =
+      isometric_projection((Vector3){cactus.pos.x, cactus.pos.y, 0});
+  DrawTextureV(rock_texture, object, WHITE);
 
   // Draw player
   int frame_count = 2;
@@ -132,9 +156,10 @@ void update_draw(void) {
   Rectangle source = {16 * current_frame, 16, 16, 16};
   Rectangle dest = {.x = projected.x,
                     .y = projected.y,
-                    .width = player.width,
-                    .height = player.height};
-  DrawTexturePro(player_sprite, source, dest, (Vector2){0, 0}, 0, player.color);
+                    .width = player.width * 32,
+                    .height = player.height * 32};
+  Vector2 origin = {player.width / 2.0f, player.height};
+  DrawTexturePro(player_sprite, source, dest, origin, -24, player.color);
 
   EndMode2D();
 
