61fa269639bba99a9908247e1da52f8c76562f8b
chrid97 <cgoncal12@gmail.com>
2025-10-10 21:33:31 -0700

    Randomized hazards - rewrite later. Also added some random stuff to make the game feel smoother but I don't think it helped, will probably remove later

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
+// (NOTE) Replace obstacle with hazard
 #include "main.h"
 #include "raylib.h"
-// #include "raymath.h"
+#include "raymath.h"
 #include <assert.h>
 #include <math.h>
 #include <stdbool.h>
@@ -21,6 +22,7 @@ const int TILE_HEIGHT = 16;
 static int current_frame = 0;
 static float frame_timer = 0.0f;
 static float shake_timer = 0.0f;
+static float last_spawn_y = 0.0f;
 
 #define MAX_OBSTACLES 100
 static Entity obstacles[MAX_OBSTACLES];
@@ -35,6 +37,13 @@ static Texture2D rock_texture;
 
 static Music bg_music;
 static Sound hit_sound;
+
+int random_between(int min, int max) {
+  if (max <= min) {
+    return min;
+  }
+  return min + rand() % (max - min + 1);
+}
 
 Vector2 isometric_projection(Vector3 pos) {
   return (Vector2){(pos.x - pos.y) * (TILE_WIDTH / 2.0f),
@@ -78,7 +87,10 @@ void init_game(void) {
 }
 
 void update_draw(void) {
-  float dt = GetFrameTime();
+  static float smooth_dt = 0.016f;
+  float raw_dt = GetFrameTime();
+  smooth_dt = Lerp(smooth_dt, raw_dt, 0.1f);
+  float dt = smooth_dt;
   UpdateMusicStream(bg_music);
 
   // scale
@@ -104,10 +116,13 @@ void update_draw(void) {
   }
 
   // --- Update ---
-
   player.pos.y -= 5 * dt;
+  // (TODO)clamp find a better way to reuse these tile values
+  player.pos.x = Clamp(player.pos.x, -5.5, 7);
   Vector2 player_screen = isometric_projection((Vector3){0, player.pos.y, 0});
-  camera.target = player_screen;
+  // camera.target = player_screen;
+  // not sure if this lerp shit makes it smoother or not
+  camera.target = Vector2Lerp(camera.target, player_screen, 10 * dt);
   camera.offset = (Vector2){GetScreenWidth() / 4.0f, GetScreenHeight() / 1.5f};
 
   if (player.damage_cooldown > 0.0f) {
@@ -115,19 +130,20 @@ void update_draw(void) {
     player.color = RED;
   }
 
-  Entity cactus = {.pos.x = 0, .pos.y = -10, .width = 1, .height = 1};
-  Rectangle cactus_rect = {cactus.pos.x, cactus.pos.y, cactus.width,
-                           cactus.height};
+  // Player-hazard collision
   Rectangle player_rect = {player.pos.x, player.pos.y, player.width,
                            player.height};
-
-  // Check collision
-  if (CheckCollisionRecs(player_rect, cactus_rect) &&
-      player.damage_cooldown <= 0) {
-    PlaySound(hit_sound);
-    player.current_health--;
-    player.damage_cooldown = 0.5f;
-    shake_timer = 0.5f;
+  for (int i = 0; i < MAX_OBSTACLES; i++) {
+    Entity *hazard = &obstacles[i];
+    Rectangle harard_rect = {hazard->pos.x, hazard->pos.y, hazard->width,
+                             hazard->height};
+    if (CheckCollisionRecs(player_rect, harard_rect) &&
+        player.damage_cooldown <= 0) {
+      PlaySound(hit_sound);
+      player.current_health--;
+      player.damage_cooldown = 0.5f;
+      shake_timer = 0.5f;
+    }
   }
 
   // Screen shake
@@ -136,6 +152,40 @@ void update_draw(void) {
     float offset_x = ((float)rand() / (float)RAND_MAX) * 2.0f - 1.0f;
     float shake_magnitude = 10.0f;
     camera.target.x += offset_x * shake_magnitude;
+  }
+
+  // spawn hazards
+  for (int i = 0; i < MAX_OBSTACLES; i++) {
+    Entity *obstacle = &obstacles[i];
+    if (obstacle->pos.y > player.pos.y + 10) {
+      last_spawn_y -= random_between(0, 10);
+      obstacle->pos.x = random_between(-5, 7);
+      obstacle->pos.y = last_spawn_y;
+
+      if (rand() % 4 == 0) {
+        int cluster_count = random_between(3, 5);
+        float cluster_offset_x = random_between(1, 3);
+        float cluster_offset_y = random_between(0, 2);
+
+        for (int j = 0; j < cluster_count; j++) {
+          for (int k = 0; k < MAX_OBSTACLES; k++) {
+            Entity *extra = &obstacles[k];
+            if (extra->pos.y > player.pos.y + 10) {
+              extra->width = 1;
+              extra->height = 1;
+              extra->pos.x = Clamp(obstacle->pos.x +
+                                       (rand() % 2 ? cluster_offset_x * j
+                                                   : -cluster_offset_x * j) +
+                                       ((rand() % 3) - 1) * 0.3f,
+                                   -5, 7);
+              extra->pos.y = obstacle->pos.y - (cluster_offset_y * j) +
+                             ((rand() % 3) - 1) * 0.5f;
+              break;
+            }
+          }
+        }
+      }
+    }
   }
 
   // --- Draw ---
@@ -163,13 +213,19 @@ void update_draw(void) {
     for (int x = -5; x < 8; x++) {
       Vector3 world = {x, y, 0};
       Vector2 screen = isometric_projection(world);
+      screen.x = floorf(screen.x);
+      screen.y = floorf(screen.y);
       DrawTextureV(tile000, screen, WHITE);
     }
   }
 
-  Vector2 object =
-      isometric_projection((Vector3){cactus.pos.x, cactus.pos.y, 0});
-  DrawTextureV(rock_texture, object, WHITE);
+  // Draw Hazards
+  for (int i = 0; i < MAX_OBSTACLES; i++) {
+    Entity *obstacle = &obstacles[i];
+    Vector2 obj =
+        isometric_projection((Vector3){obstacle->pos.x, obstacle->pos.y, 0});
+    DrawTextureV(rock_texture, obj, WHITE);
+  }
 
   // Draw player
   int frame_count = 2;
@@ -183,10 +239,10 @@ void update_draw(void) {
   Rectangle source = {16 * current_frame, 16, 16, 16};
   Rectangle dest = {.x = projected.x,
                     .y = projected.y,
-                    .width = player.width * 32,
-                    .height = player.height * 32};
+                    .width = player.width * 28,
+                    .height = player.height * 28};
   Vector2 origin = {player.width / 2.0f, player.height};
-  DrawTexturePro(player_sprite, source, dest, origin, -24, player.color);
+  DrawTexturePro(player_sprite, source, dest, origin, 0, player.color);
 
   EndMode2D();
 
