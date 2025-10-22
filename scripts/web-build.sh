ce8c2e9aec864df656a9edee3ae523a2d126e208
chrid97 <cgoncal12@gmail.com>
2025-10-21 17:17:02 -0700

    update web build script

--- scripts/web-build.sh
+++ scripts/web-build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
 source ~/third-party/emsdk/emsdk_env.sh
 
-emcc -o build/yeehaw.html \
+mkdir -p build
+
+emcc -o build/game.html \
   src/platform_web.c src/game.c \
   -s USE_GLFW=3 \
   -s ASYNCIFY \
   -s INITIAL_MEMORY=64MB \
   -s ALLOW_MEMORY_GROWTH=1 \
+  -s STACK_SIZE=256KB \
   -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
   -s FORCE_FILESYSTEM=1 \
   -DPLATFORM_WEB \
